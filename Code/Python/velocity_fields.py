63964539c8b0427c0dce26f999ad0af6610c7863
Callum Shaw <callum.shaw@anu.edu.au>
2020-03-13 16:52:20 +1100

    Update velocity_fields.py

--- Code/Python/velocity_fields.py
+++ Code/Python/velocity_fields.py
@@ -14,9 +14,28 @@ import matplotlib.pyplot as plt
 import concurrent.futures
 
 
-data_path='E:/batch 2/3echo/run8/transit/results/data.npz'
-data = np.load(data_path, allow_pickle=True)
+data_path='E:/batch 2/3echo/run8/transit/results/centre_data.npz'
+data = np.load(data_path)
 
+rho = data['centre_rho']
 
+#remving nans at base
+min_nan=np.min(np.sum(np.isnan(rho[0]),axis=0)) #summing the Nans in the vertical direction
+offset = 15
+rho_c = rho[:,:-(min_nan+offset),:]
 
-def topograghy()
+def topograghy(rho, no_hills = 1):
+    base = rho[0]
+    y,x = base.shape
+    domain = np.arange(x)
+    
+    if no_hills == 1:
+        nan_array = np.sum(np.isnan(base),axis=0)
+        max_amp = np.max(nan_array)
+        max_loc = np.argmax(nan_array)
+        
+        h_m = max_amp//2
+        h_m_array  = base[h_m,:]
+        h_m_w = np.sum(h_m_array)//2
+        
+    if no_hills == 2:
