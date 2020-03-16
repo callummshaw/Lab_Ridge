4b77a2ef7db43aa2025efe357054323970eb523b
Callum Shaw <callum.shaw@anu.edu.au>
2020-03-17 10:08:40 +1100

    Create fourier_filtering.py
    New program for filtering the transformed data to remove noise

---
+++ Code/Python/fourier_filtering.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 10:06:42 2020
+
+@author: Callum Shaw
+"""
+
+import numpy as np
+import analysis_functions as af
+from multiprocessing import Pool
