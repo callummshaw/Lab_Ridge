b3fefbf50f30285c94e49fe293711aeb1f5ed100
Callum Shaw <callum.shaw@anu.edu.au>
2020-03-16 16:55:06 +1100

    Added multiprocessing
    Added multiprocessing

--- Code/Python/velocity_fields.py
+++ Code/Python/velocity_fields.py
@@ -11,91 +11,28 @@ continuity equations.
 
 import numpy as np
 import matplotlib.pyplot as plt
-import concurrent.futures
 import analysis_functions as af
+from multiprocessing import Pool
+from scipy import interpolate 
 
-#data_path='E:/batch 2/3echo/run8/transit/results/centre_data.npz'
-#data = np.load(data_path)
+data_path='E:/batch 2/3echo/run8/transit/results/centre_data.npz'
+data = np.load(data_path)
 
-#rho = data['centre_rho']
+rho = data['centre_rho']
 
 #remving nans at base
-#in_nan=np.min(np.sum(np.isnan(rho[0]),axis=0)) #summing the Nans in the vertical direction
-#offset = 15
-#rho_c = rho[:,:-(min_nan+offset),:]
+min_nan=np.min(np.sum(np.isnan(rho[0]),axis=0)) #summing the Nans in the vertical direction
+offset = 15
 
+rho_c = rho[:,:-(min_nan+offset),:]
 
     
-def topograghy_mask(rho, no_hills, lensing):
-    '''
-    A function that reads in a dataset and returns a function that can be used to mask
-    the topography (needed for fourier transform)
 
-    Parameters
-    ----------
-    rho : Dataset that is being read in
-    no_hills : The number of hills in the topo. The default is 1.
-    lensing : A sort of fudge factor- to overcome the effect of lensing on the topo
-        The default is 33.
-
-    Returns
-    -------
-    topo_function : An funtion in the form of a 1D array that can be used to mask
-        the topography.
-
-    '''
-    t,y,x = rho.shape
-    base = rho[t//2]
-    domain = np.arange(x)
-    
-
-    if no_hills == 1:
-        
-        height_increase=20 #pixels we want to increase the topo height by (otherwise cuts off sides of the top)
-        
-        max_amp, max_loc = af.max_and_loc(base)
-        
-        h_m = max_amp//2
-        h_m_array  = base[-h_m,:]*0+1
-        h_m_w = (x-np.nansum(h_m_array))//2+lensing
-        
-        max_amp = max_amp + height_increase
-        
-        topo_function=-max_amp*np.exp(-(domain-max_loc)**2/(2*h_m_w**2))+y
-        
-        return topo_function
-        
-    if no_hills == 2:
-        
-        print('Currently Untested')
-        
-        mid = x//2
-        
-        side1 = base[:, :mid]
-        side2 = base[:, mid:]
-    
-        #splitting the domain in two parts to find the hills, will assume hills are the same shape, 
-        #only difference is a horizontal translation
-    
-        height_increase=20
-        
-        max_amp_1, max_loc_1 = af.max_and_loc(side1)
-        max_amp_2, max_loc_2 = af.max_and_loc(side2)
-    
-        
-        
-        h_m = max_amp_1//2
-        h_m_array  = base[-h_m,:]*0+1
-        h_m_w = (x-np.nansum(h_m_array))//2+lensing
-        
-        topo_function=-max_amp_1*np.exp(-(domain-max_loc_1)**2/(2*h_m_w**2))-max_amp_2*np.exp(-(domain-max_loc_2)**2/(2*h_m_w**2))+y
-         
-        return topo_function
   
 def transformation(data, no_hills=1, lensing=33):
    
     t,z,x=data.shape
-    topo_function = topograghy_mask(data, no_hills, lensing)
+    topo_function = af.topograghy_mask(data, no_hills, lensing)
     
     #creating a meshgrid of pixel locations
     x_array = np.arange(x)
@@ -125,3 +62,39 @@ def transformation(data, no_hills=1, lensing=33):
     transformed[transformed==0]=np.nan
     
     return transformed
+
+transformed_data = transformation(rho_c)
+
+
+def interp_nan(time):
+    data = transformed_data
+    t,z,x=data.shape
+    
+    #creating a meshgrid of pixel locations
+    x_array = np.arange(x)
+    z_array = np.arange(z)
+    xx,zz=np.meshgrid(x_array,z_array)
+    
+    
+    array = np.ma.masked_invalid(data[time])
+    x1=xx[~array.mask]
+    z1=zz[~array.mask]
+    newarr=array[~array.mask]
+
+    interp = interpolate.griddata((x1, z1), newarr.ravel(),(xx, zz), method='cubic')
+    return interp
+ 
+
+
+if __name__ == '__main__':
+   
+    numbers=np.arange(transformed_data.shape[0])
+    p=Pool()
+    result = p.map(interp_nan, numbers)
+    p.close()
+    p.join()
+
+ 
+
+
+
