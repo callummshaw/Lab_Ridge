bb795992265b1b981818d6a6506f635a351db81e
Callum Shaw <callum.shaw@anu.edu.au>
2020-03-16 11:19:33 +1100

    Update velocity_fields.py
    Fixing the topography finding function

--- Code/Python/velocity_fields.py
+++ Code/Python/velocity_fields.py
@@ -13,29 +13,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import concurrent.futures
 
+#data_path='E:/batch 2/3echo/run8/transit/results/centre_data.npz'
+#data = np.load(data_path)
 
-data_path='E:/batch 2/3echo/run8/transit/results/centre_data.npz'
-data = np.load(data_path)
-
-rho = data['centre_rho']
+#rho = data['centre_rho']
 
 #remving nans at base
-min_nan=np.min(np.sum(np.isnan(rho[0]),axis=0)) #summing the Nans in the vertical direction
-offset = 15
-rho_c = rho[:,:-(min_nan+offset),:]
+#min_nan=np.min(np.sum(np.isnan(rho[0]),axis=0)) #summing the Nans in the vertical direction
+#offset = 15
+#rho_c = rho[:,:-(min_nan+offset),:]
 
-def topograghy(rho, no_hills = 1):
-    base = rho[0]
-    y,x = base.shape
+def topograghy_mask(rho, no_hills=1,lensing=33 ):
+    t,y,x = rho.shape
+    base = rho[t//2]
     domain = np.arange(x)
     
     if no_hills == 1:
+        
+        height_increase=20 #pixels we want to increase the topo height by (otherwise cuts off sides of the top)
+        
         nan_array = np.sum(np.isnan(base),axis=0)
         max_amp = np.max(nan_array)
-        max_loc = np.argmax(nan_array)
+        nan_array = np.float32(nan_array) #need to convert from int to float
+        nan_array[nan_array<max_amp-5]=np.nan #removing everything but the top of the hill
+        
+        nan_count=nan_array*0+1
+        nan_count=nan_count*np.arange(x) #creating an array where the value is the index
+        max_loc = np.nanmean(nan_count) #finding the average index of top of the hill
+     
         
         h_m = max_amp//2
-        h_m_array  = base[h_m,:]
-        h_m_w = np.sum(h_m_array)//2
+        h_m_array  = base[-h_m,:]*0+1
+        h_m_w = (x-np.nansum(h_m_array))//2+lensing
+        
+        max_amp = max_amp + height_increase
+        
+        topo_function=-max_amp*np.exp(-(domain-max_loc)**2/(2*h_m_w**2))+y
+        
         
     if no_hills == 2:
+        print('unfinished')
+    
+    
+    return topo_function
