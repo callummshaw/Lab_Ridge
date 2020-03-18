2c4c134e5ce215507842944e1c8c265f9cce5b55
Callum Shaw <48504663+callummshaw@users.noreply.github.com>
2020-03-18 17:42:46 +1100

    Parellelised fft
    Still having memory issues saving array

--- Code/Python/fourier_filtering.py
+++ Code/Python/fourier_filtering.py
@@ -6,8 +6,10 @@ Created on Tue Mar 17 10:06:42 2020
 """
 from numpy import fft
 import numpy as np
+from multiprocessing.pool import ThreadPool as Pool
+import time
 #import analysis_functions as af
-
+st = time.time()
 data_path = 'D:/batch 2/3echo/run8/transit/results/transformation_result.npz'
 data = np.load(data_path)
 
@@ -15,7 +17,21 @@ t_data = data['result']
 t_data = np.moveaxis(t_data,2,0) #move time to first axis like rest of data
 data = t_data[:,1:-1,1:-1] #removing boundary nans
 
-def fourier_filter(data,sigma=.1):
+#filter:
+mu=0
+sigma=.1
+
+t,z,x = data.shape
+
+x_fft = np.fft.fftfreq(x)
+z_fft = np.fft.fftfreq(z)
+
+x_filt =np.exp(-(x_fft-mu)**2/(2*sigma**2))
+z_filt =np.exp(-(z_fft-mu)**2/(2*sigma**2))
+
+filt = x_filt*z_filt[:,None]
+
+def fourier_filter(i):
     '''
     A function the preforms low pass filter, removing all the high frequency
     noise present in the data (using a gaussian filter).
@@ -31,19 +47,27 @@ def fourier_filter(data,sigma=.1):
     Data set that has undergone low pass filter
 
     '''
-    mu=0 #mean of guassian filter should be kept to zero
+    cropped = data[i]
+   #mean of guassian filter should be kept to zero
     
     #building filter
-    t,z,x = data.shape
-    x_fft = np.fft.fftfreq(x)
-    z_fft = np.fft.fftfreq(z)
-    x_filt =np.exp(-(x_fft-mu)**2/(2*sigma**2))
-    z_filt =np.exp(-(z_fft-mu)**2/(2*sigma**2))
-    filt = x_filt*z_filt[:,None]
     
-    fft_data = fft.fft2(data) #preforming fft
+    
+    fft_data = fft.fft2(cropped) #preforming fft
     filtered_data = fft_data*filt #filtering
     ifft_data = fft.ifft2(filtered_data) #going back to cartesian space
     
     return np.float32(ifft_data.real)
 
+if __name__ == '__main__':
+      numbers=range(t)
+      p = Pool()
+      result = p.map(fourier_filter, numbers)
+      p.close()
+      p.join()
+
+result=np.dstack(result)
+result = np.moveaxis(result,2,0)
+et = time.time()
+tt=et-st
+print(f'This process took {tt} seconds')
