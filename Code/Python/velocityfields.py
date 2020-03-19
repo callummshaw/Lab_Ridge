03ee35d8d42993825af7d78c046314d49d309e57
Callum Shaw <callum.shaw@anu.edu.au>
2020-03-19 13:57:58 +1100

    Update velocityfields.py
    Added transformation back to include topography

--- Code/Python/velocityfields.py
+++ Code/Python/velocityfields.py
@@ -8,8 +8,29 @@ import time
 import numpy as np
 from multiprocessing.pool import ThreadPool as Pool
 import analysis_functions as af
+import matplotlib.pyplot as plt
 
 transform_data_path='E:/batch 2/3echo/run8/transit/results/centre_data.npz'
-filtered_data_path='E:/batch 2/3echo/run8/transit/results/filtered_data.np'
+filtered_data_path='E:/batch 2/3echo/run8/transit/results/filtered_data.npy'
+unfiltered_data_path='E:/batch 2/3echo/run8/transit/results/centre_data.npz'
+uf=np.load(unfiltered_data_path)
+rho = uf['centre_rho']
 
 zt = af.transformed_coords(transform_data_path, return_dataset='no')
+filtered_data =  np.load(filtered_data_path)
+zt=zt[1:-1,1:-1]
+z,x=zt.shape
+
+tester = filtered_data[0]
+dummy = np.zeros(zt.shape)
+
+for i in range(x): 
+    for j in range(z):
+        z_val = zt[j,i]
+        if z_val < z-1:
+            dummy[j,i]=tester[int(z_val),i]
+dummy=np.float32(dummy)
+dummy[dummy==0]=np.nan       
+
+plt.imshow(dummy)
+plt.colorbar()
